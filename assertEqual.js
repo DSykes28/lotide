24eb2d54e97e4d17221e66074822d58c4c323ee8
David <sykesdavid8@gmail.com>
2020-08-17 19:57:22 +0000

    Assert Equal

---
+++ assertEqual.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+
+  if(actual === expected) {
+
+    console.log('💚💚💚 Assertion Passed: ' + actual + ' === ' + expected)
+
+  } else {
+
+    console.log('😡😡😡 Assertion Failed: ' + actual + ' !== ' + expected)
+  }
+
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual('Batman', 'batman');
+assertEqual(2344, 2344);
+assertEqual('Cyberpunk', 'Cyberpunk');
+assertEqual(false, false);
