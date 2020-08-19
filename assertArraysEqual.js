f24698ed5388fa3db081ecfa611835184bda050e
David <sykesdavid8@gmail.com>
2020-08-19 20:16:02 +0000

    cleaned up the code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,9 +1,13 @@
-const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(array1, array2) {
+ 
   if (eqArrays(array1, array2)) {
+ 
     console.log(`💚  Assertion passed: ${array1} === ${array2}`);
+ 
   } else {
+ 
     console.log(`😡  Assertion failed: ${array1} !== ${array2}`);
+ 
   }
 };
