2017350530aba0c6448fbcf6a4cf0505da366e8d
David <sykesdavid8@gmail.com>
2020-08-19 19:45:25 +0000

    code added

---
+++ countLetters.js
@@ -0,0 +1,36 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+
+    console.log(`💚💚💚 Assertion Passed: ${actual}  === ${expected}`);
+
+  } else {
+
+    console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const countLetters = function(string) {
+
+  let cleanedString = string.split(" ").join('');
+  
+  let results = {};
+  
+  for (let letter of cleanedString) {
+  
+    if (results[letter]) {
+      
+      results[letter] += 1 
+
+    } else {
+      
+      results[letter] = 1;
+    }
+  }
+
+  return results;
+
+};
+
+console.log(countLetters('This sentence is not very long'));
