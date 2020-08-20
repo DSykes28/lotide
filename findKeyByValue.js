0562c9147f4d1f60640a7e841c713e221f8f972f
David <sykesdavid8@gmail.com>
2020-08-20 00:48:47 +0000

    code added

---
+++ findKeyByValue.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+
+    console.log(`💚💚💚 Assertion Passed: ${actual}  === ${expected}`);
+
+  } else {
+
+    console.log(`😡😡😡 Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findKeyByValue = function(category, value) {
+
+  for (let key in category) {
+  
+    if (value === category[key]) {
+  
+      return key;
+  
+    }
+  }
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
