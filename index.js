74006b748b400e12069a370c1993a346e590697b
David <sykesdavid8@gmail.com>
2020-08-24 20:28:54 +0000

    shortened

--- index.js
+++ index.js
@@ -16,19 +16,19 @@ const without  = require('./without');
 
 
 module.exports = {
-  assertArraysEqual: assertArraysEqual,
-  assertEqual: assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countLetters: countLetters,
-  countOnly: countOnly,
-  eqArrays: eqArrays,
-  eqObjects: eqObjects,
-  findKey: findKey,
-  findKeyByValue: findKeyByValue,
-  head:   head,
-  letterPositions: letterPositions,
-  middle: middle,
-  tail:   tail,
-  takeUntil: takeUntil,
-  without: without
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  head,
+  letterPositions,
+  middle,
+  tail,
+  takeUntil,
+  without
 };
